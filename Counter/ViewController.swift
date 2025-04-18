20f624f294ba9982e415ea6dc45c9160add62667
Anastasia <anastasiaserokurova@gmail.com>
2025-04-18 13:50:27 +0700

    Добавила кнопку и счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countTouchLabel: UILabel!
+    @IBOutlet weak var counterButton: UIView!
+    
+    private var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countTouchLabel.text = "Значение счётчика: \(count)"
+    
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        count += 1
+        countTouchLabel.text = "Значение счётчика: \(count)"
+    }
 }
-
