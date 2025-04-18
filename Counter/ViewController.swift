b2a595129366bbebe75127cb12b5ecbfcbadc0ba
Anastasia <anastasiaserokurova@gmail.com>
2025-04-18 13:18:28 +0700

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Nastya Adodina on 18.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
