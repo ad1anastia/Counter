8f7ba197deefde7bea81820920abfdbd2f38df11
Anastasia <anastasiaserokurova@gmail.com>
2025-04-19 11:34:05 +0700

    Усложнённая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var countTouchLabel: UILabel!
-    @IBOutlet weak var counterButton: UIView!
+    @IBOutlet weak var historyTextView: UITextView!
     
     private var count = 0
     
@@ -18,11 +18,48 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         countTouchLabel.text = "Значение счётчика: \(count)"
-    
+        historyTextView.text = "История изменений:\n"
     }
-
-    @IBAction func buttonTapped(_ sender: Any) {
+    
+    @IBAction func incrementCounter(_ sender: UIButton) {
         count += 1
+        updateHistory(with: "Значение изменено на +1")
+        updateCounterLabel()
+    }
+    
+    @IBAction func decrementCounter(_ sender: UIButton) {
+        if count > 0 {
+            count -= 1
+            updateHistory(with: "Значение изменено на -1")
+        } else {
+            updateHistory(with: "Попытка уменьшить значение счётчика ниже 0")
+        }
+        updateCounterLabel()
+    }
+    
+    @IBAction func resetCounter(_ sender: UIButton) {
+        count = 0
+        updateHistory(with: "Значение сброшено")
+        updateCounterLabel()
+    }
+    
+    private func updateCounterLabel() {
         countTouchLabel.text = "Значение счётчика: \(count)"
+      }
+    
+    private func updateHistory(with changeDescription: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let timestampString = dateFormatter.string(from: Date.now)
+        
+        let newEntry = "[\(timestampString)] : \(changeDescription)\n"
+        
+        historyTextView.text += newEntry
+        scrollToBottom()
+    }
+    
+    private func scrollToBottom() {
+        let bottom = NSRange(location: historyTextView.text.count - 1, length: 1)
+        historyTextView.scrollRangeToVisible(bottom)
     }
 }
